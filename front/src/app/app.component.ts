733ff59c4301d09a4c463ad3952bde196350aa83
Cristian Marin <cmarinb@ucentral.edu.co>
2024-05-07 19:32:47 -0500

    Cambios frontend backend

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,29 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
-export class AppComponent{
+export class AppComponent {
   title = 'front';
-
-  ngOnInit(): void {
-
-  }
-
-  test() {
-    Swal.fire({
-      title: 'Error!',
-      text: 'Do you want to continue',
-      icon: 'error',
-      confirmButtonText: 'Cool'
-    })
-  }
 }
-
-
--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.css'
 })
+
+
 export class LoginComponent {
+onSubmit() {
+
+}
 
 }
