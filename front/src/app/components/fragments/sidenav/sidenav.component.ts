659325321d738ceac574ff2c60c4506f1d3108e0
Cristian Marin <cmarinb@ucentral.edu.co>
2024-05-25 10:55:10 -0500

    Environments

--- front/src/app/components/fragments/sidenav/sidenav.component.ts
+++ front/src/app/components/fragments/sidenav/sidenav.component.ts
@@ -1,12 +1,41 @@
+import {MediaMatcher} from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import {ChangeDetectorRef, OnDestroy} from '@angular/core'
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatSidenavModule],
+  imports: [MatSidenavModule, MatListModule,MatToolbarModule, MatIconModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
+  mobileQuery: MediaQueryList;
 
+  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+
+  fillerContent = Array.from(
+    {length: 50},
+    () =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+  );
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
