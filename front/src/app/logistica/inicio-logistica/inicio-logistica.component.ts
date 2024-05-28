040d2055f0b9238349edfab5bd253ca71db12f9d
Cristian Marin <cmarinb@ucentral.edu.co>
2024-05-27 22:11:24 -0500

    ventana marca

--- front/src/app/logistica/inicio-logistica/inicio-logistica.component.ts
+++ front/src/app/logistica/inicio-logistica/inicio-logistica.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import { ConsultarComponent } from './consultar/consultar.component';
 import { AlmacenarInventarioComponent } from './almacenar-inventario/almacenar-inventario.component';
+import { CommonModule } from '@angular/common';
+import { Usuario } from '../../models/usuario/usuario';
 
 @Component({
   selector: 'app-inicio-logistica',
   standalone: true,
-  imports: [MatTabsModule, ConsultarComponent, AlmacenarInventarioComponent],
+  imports: [MatTabsModule, ConsultarComponent, AlmacenarInventarioComponent, CommonModule],
   templateUrl: './inicio-logistica.component.html',
   styleUrl: './inicio-logistica.component.css'
 })
 export class InicioLogisticaComponent {
+  user: Usuario = new Usuario();
 
+  ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('usuario') as string);
+  }
 }
