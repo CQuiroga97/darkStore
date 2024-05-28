779842083bcf047e058c8de83c5782d1cdc22476
Jhomove <jhonatan.moreno@wepay4u.com>
2024-05-28 00:01:03 -0500

    Feat: servicio para listar categorias producto

--- back/producs-microservice/src/entities/category.entity.ts
+++ back/producs-microservice/src/entities/category.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
 import { Product } from './product.entity';
 
 @Entity('categoria_productos')
@@ -6,6 +13,17 @@ export class Category {
     @PrimaryGeneratedColumn({ name: 'id_categoria_producto' })
     id: number;
 
+    @Column({ name: 'nombre' })
+    name: string;
+
+    @Column({ name: 'descripcion' })
+    description: string;
+
+    @CreateDateColumn({ name: 'fecha_creacion' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: 'fecha_actualizacion' })
+    updatedAt: Date;
     //relationships
     @OneToMany(() => Product, (product) => product.category)
     products: Product[];
