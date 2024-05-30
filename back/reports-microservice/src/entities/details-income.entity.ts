01a36b1027e4f6a905288312e9141bef05663189
CQuiroga97 <quiroga.cristian97@hotmail.com>
2024-05-30 13:39:35 -0500

    Ajuste de ecosystem

--- back/reports-microservice/src/entities/details-income.entity.ts
+++ back/reports-microservice/src/entities/details-income.entity.ts
@@ -22,9 +22,9 @@ export class IncomeDetail {
     @Column({ name: 'cantidad' })
     quantity: number;
 
-    @ManyToOne(() => Product, (product: Product) => product.incomeDetails)
-    @JoinColumn({ name: 'producto_id' })
-    product: Product;
+    // @ManyToOne(() => Product, (product: Product) => product.incomeDetails)
+    // @JoinColumn({ name: 'producto_id' })
+    // product: Product;
 
     @ManyToOne(() => Income, (income: Income) => income.incomeDetails)
     @JoinColumn({ name: 'ingreso_id' })
